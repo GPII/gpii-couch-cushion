701fbbd4aade9bb5c118c3fb3362d88cd30eda15
Tony Atkins <tony@raisingthefloor.org>
2016-03-10 16:45:44 +0100

    GPII-1253:  Added documentation for `fluid.transforms.transformEach`.

--- src/js/lib/transformEach.js
+++ src/js/lib/transformEach.js
@@ -1,6 +1,9 @@
 /*
 
-    A `fluid.standardOutputTransformFunction` to transform each element of an array or map using standard transformation rules.
+    A `fluid.standardOutputTransformFunction` to transform each element of an array or map using standard transformation
+    rules. See the documentation for details:
+
+    https://github.com/the-t-in-rtf/gpii-couch-cushion/blob/GPII-1253/docs/transformEach.md
 
     TODO:  Discuss submitting this as a PR to be added to: https://github.com/fluid-project/infusion/blob/master/src/framework/core/js/ModelTransformationTransforms.js
 
@@ -12,6 +15,13 @@ fluid.defaults("fluid.transforms.transformEach", {
     gradeNames: "fluid.standardTransformFunction"
 });
 
+/**
+ *
+ * @param valuesToTransform {Object}  The array or map of raw values.
+ * @param transformSpec {Object} The transform configuration passed to us by the `StandardTransformFunction` infrastructure.
+ * @returns {Object} The transformed array or map.
+ *
+ */
 fluid.transforms.transformEach = function (valuesToTransform, transformSpec) {
     var transformedArray = Array.isArray(valuesToTransform) ? [] : {};
     fluid.each(valuesToTransform, function (valueToTransform, key) {
